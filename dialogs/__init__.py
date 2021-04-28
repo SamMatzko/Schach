145d326116670e7eef555ea364459783fe0d896a
Samuel Matzko <cyal8r@yesU2.com>
2021-04-28 16:47:15 -0400

    Added a dialogs module and a PromotionDialog in it. Will add more dialogs later.

---
+++ dialogs/__init__.py
@@ -0,0 +1,117 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+
+from constants import *
+from gi.repository import Gtk
+
+class PromotionDialog(Gtk.Dialog):
+    """The dialog that shows when a piece is promoted."""
+
+    def __init__(self, parent, color="white"):
+        Gtk.Dialog.__init__(
+            self,
+            title="Promotion!",
+            transient_for=parent,
+            modal=True
+        )
+        self.set_resizable(False)
+
+        # The color of the piece we are promoting
+        self.color = color
+
+        # The area to which we can add the button box
+        self.area = self.get_content_area()
+
+        # The variable to return, containing the piece to promote to.
+        # Default is queen.
+        if self.color == "white":
+            self.variable_to_return = "Q"
+        else:
+            self.variable_to_return = "q"
+
+        # The button box
+        self.box = Gtk.HBox()
+        self.area.add(self.box)
+
+        # Create the buttons and their images
+        self._create_buttons()
+
+        # Show all
+        self.show_all()
+
+    def _create_buttons(self):
+        """Create the buttons and assign their images."""
+
+        if self.color == "white":
+            queen_image = IMAGE_Q
+            rook_image = IMAGE_R
+            bishop_image = IMAGE_B
+            knight_image = IMAGE_N
+        else:
+            queen_image = IMAGE_q
+            rook_image = IMAGE_r
+            bishop_image = IMAGE_b
+            knight_image = IMAGE_n
+
+        # The queen_button
+        self.queen_button = Gtk.Button()
+        self.queen_button.connect("clicked", self._on_click, "q")
+        self.queen_image = Gtk.Image.new_from_file(queen_image)
+        self.queen_button.set_image(self.queen_image)
+        self.box.add(self.queen_button)
+
+        # The rook_button
+        self.rook_button = Gtk.Button()
+        self.rook_button.connect("clicked", self._on_click, "r")
+        self.rook_image = Gtk.Image.new_from_file(rook_image)
+        self.rook_button.set_image(self.rook_image)
+        self.box.add(self.rook_button)
+
+        # The bishop_button
+        self.bishop_button = Gtk.Button()
+        self.bishop_button.connect("clicked", self._on_click, "b")
+        self.bishop_image = Gtk.Image.new_from_file(bishop_image)
+        self.bishop_button.set_image(self.bishop_image)
+        self.box.add(self.bishop_button)
+
+        # The knight_button
+        self.knight_button = Gtk.Button()
+        self.knight_button.connect("clicked", self._on_click, "n")
+        self.knight_image = Gtk.Image.new_from_file(knight_image)
+        self.knight_button.set_image(self.knight_image)
+        self.box.add(self.knight_button)
+
+    def _on_click(self, widget, peice):
+        """Set the varaible-to-return to the piece that was clicked."""
+
+        # Destroy the dilaog
+        self.destroy()
+
+        # Set the variable-to-return
+        if self.color == "white":
+            self.variable_to_return = peice.upper()
+        else:
+            self.variable_to_return = peice.lower()
+
+    def show_dialog(self):
+        """Run the dilaog and return the user's response."""
+
+        # Run the dialog
+        self.run()
+
+        # Return the variable
+        return self.variable_to_return
+
+if __name__ == "__main__":
+    window = Gtk.Window()
+    window.maximize()
+    window.add(Gtk.Label(label="Press a key to see the dialogs."))
+    window.show_all()
+    def sd(*args):
+        print(PromotionDialog(window).show_dialog())
+        print(PromotionDialog(window, "black").show_dialog())
+
+    window.connect("delete-event", Gtk.main_quit)
+    window.connect("key-press-event", sd)
+    Gtk.main()
