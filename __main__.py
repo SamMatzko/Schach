18f1bef9cf4e41b631fdd73ddbb587f636860cc7
Samuel Matzko <cyal8r@yesU2.com>
2021-04-29 16:45:33 -0400

    Added appinfo.json and made __main__.py print the version when the app runs.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,15 @@
 import app
 import gi
+import json
 
 gi.require_version("Gtk", "3.0")
 
+from constants import *
 from gi.repository import Gtk
+
+# Print the Scach version
+appinfo = json.load(open(f"{ROOT_PATH}json/appinfo.json"))
+print(f"Schach {appinfo['version']}")
 
 if __name__ == "__main__":
     window = app.App()
