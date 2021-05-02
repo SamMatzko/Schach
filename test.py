eb594288f85df0c86e13dbf22c43feb77157abe5
Samuel Matzko <cyal8r@yesU2.com>
2021-05-02 15:11:02 -0400

    Added a class that tests the game manager.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import chess
 import chessboard as cb
+import game
 import gi
 import unittest
 
@@ -8,6 +9,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 BOARD_STRING = "2b4r/p3p1pp/1p2q3/Q1pn2kP/2bPnp2/1P5p/P1PrBP1P/RN2KBNR w KQkq - 0 1"
+CHECKMATE = "4k3/8/8/8/8/8/1r6/Kq6 w KQkq - 0 67"
 
 class SchachTestCase(unittest.TestCase):
     """The TestCase for Schach."""
@@ -31,6 +33,37 @@ class SchachTestCase(unittest.TestCase):
 
         self.assertEqual(str(game).replace("\n", " "), chessboard.get_board_string())
 
+        window.destroy()
+
+class GameManagerTestCase(unittest.TestCase):
+    """Test game.py."""
+
+    def create_test_chessboard(self):
+        """Create a chessboard.ChessBoard widget for testing."""
+        window = Gtk.Window()
+        chessboard = cb.ChessBoard(window)
+        window.add(chessboard)
+        return window, chessboard
+
+    def create_test_environment(self):
+        window, chessboard = self.create_test_chessboard()
+        board_game = chess.Board(CHECKMATE)
+        chessboard.from_string(str(board_game).replace("\n", " "))
+        game_manager = game.Game(window, chessboard, None, None)
+        game_manager.board = board_game
+        game_manager.engine.quit()
+        return game_manager, chessboard, window
+
+    def test_board(self):
+        game_manager, chessboard, window = self.create_test_environment()
+        self.assertEqual(str(game_manager.board).replace("\n", " "), chessboard.get_board_string())
+        window.destroy()
+
+    def test_game_status(self):
+        game_manager, chessboard, window = self.create_test_environment()
+        self.assertEqual(game_manager.board.is_game_over(), True)
+        self.assertEqual(game_manager.dialog_ok, False)
+        self.assertEqual(game_manager.board.is_checkmate(), True)
         window.destroy()
 
 if __name__ == "__main__":
