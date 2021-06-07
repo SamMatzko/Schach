400da71d9d74bde13b2332e6ea9d1a2fcff52581
Samuel Matzko <cyal8r@yesU2.com>
2021-06-07 16:37:24 -0400

    Started an install system.

--- install.py
+++ install.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # Schach is a basic chess application that uses the Stockfish chess engine.
 # Copyright (C) 2021  Samuel Matzko
 
@@ -20,34 +23,41 @@
 
 """Install Schach."""
 
+import json
 import os
 import sys
 
 # Install only if we are on a linux-based system
 if "linux" in sys.platform.lower():
 
-    # Our location 
-    CURRENT_DIR = os.path.dirname(__file__) + "/"
-
-    # The install directory
+    # The directories
+    APPLICATION = os.path.dirname(__file__)
+    APPLICATION_DIR = APPLICATION + "/"
+    CURRENT_DIR = os.path.dirname(APPLICATION_DIR)
     INSTALL_IN = os.environ["HOME"] + "/.local/share/"
 
-    # The directory for the desktop file
-    DESKTOP_DIR = os.environ["HOME"] + "/.local/share/applications/"
+    # Load our version info
+    with open(APPLICATION_DIR + "json/appinfo.json") as f:
+        current_appinfo = json.load(f)
+        f.close()
+    current_version = current_appinfo["version"]
+    current_update = current_appinfo["nano_revision"]
 
-    # The contents of the desktop file
-    f"""[Desktop Entry]
-Name=Schach
-Comment=A chess-playing app that uses the stockfish engine
-GenericName=Schach chess app
-Exec=python3 {INSTALL_IN}Schach/ %F
-Icon={INSTALL_IN}Schach/icons/application/appicon.png
-Type=Application
-Terminal=True
-StartupNotify=True
-Keywords=chess;master;"""
-    print("This does not install Schach yet.")
+    # Install the app in the directory
+    os.rename(APPLICATION, "%sSchach %s" % (CURRENT_DIR, ("%s.%s" % (current_version, str(current_update)))))
 
+    # # Load the version info from the already-installed Schach, if it exists
+    # if os.path.exists(INSTALL_IN + "Schach/json/appinfo.json"):
+    #     with open(INSTALL_IN + "Schach/json/appinfo.json") as f:
+    #         old_appinfo = json.load(f)
+    #         f.close()
+    #     old_version = old_appinfo["version"]
+    #     old_update = old_appinfo["nano_revision"]
+
+    #     if old_version == current_version:
+    #         if old_update == current_update:
+    #             print("You already have this version of Schach installed.")
+    #             exit()
 
 else:
-    print("FATAL ERROR: This program can only install in Linux systems. Install aborted.")
+    print("FATAL ERROR: Your system is not a Linux. This install cannot run.")
